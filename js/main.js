a0ff022a1ca16954a5186a50eaea577be381f942
ManeevDumitru <shanegrimes@mail.ru>
2019-12-21 23:36:06 +0200

    Added Some more

--- js/main.js
+++ js/main.js
@@ -1,70 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
     audioSound();
-    translateWhenScrollKojima();
-    scrollToEllement();
-    scrollToMG();
-    scrollToSnatcher();
-    scrollToMGS5TPP();
+    ticking();
+    testing();
 });
 
-function audioSound (){
-    $('audio').prop("volume",0.1);  
+function audioSound() {
+    $('audio').prop("volume", 0.1);
 }
-
-/*function translateWhenScrollKojima(){
-    var scroll = $(window).scrollTop();
-    $('.sectionHideoKojima').css({
-        transform: 'translate3d(-50px, 0)'
-    });
-}
-
-function scrollToMG() {
-    $("#MG").click(function (){
-        var audio = document.getElementById('mG1987');
-        $('html, body').animate({
-            scrollTop: $(".sectionMG1987").offset().top
-        },1);
-        audio.play();
-        setTimeout(function() {
-            var audio = document.getElementById("mG1987"); 
-            $('audio').prop("volume",0.02);  
-        },8000);
-    });
-}
-
-function scrollToSnatcher() {
-    $("#Sn").click(function (){
-        var audio = document.getElementById('sNatcher');
-        $('html, body').animate({
-            scrollTop: $(".sectionSnatcher").offset().top
-        },1);
-        audio.play();
-        setTimeout(function() {
-            var Audio = document.getElementById("narutoo"); 
-            Audio.pause();
-        },2200);
-    });
-}
-
-function scrollToMGS5TPP() {
-    $("#MGS5TPP").click(function (){
-        var audio = document.getElementById('mGS5TPP');
-        $('html, body').animate({
-            scrollTop: $(".sectionMGS5TPP").offset().top
-        },1);
-        audio.play();
-    });
-}*/
 
 /* ///////////////////////////// */
 
-document.getElementById('button').addEventListener("click", goAway);
-function goAway() {
-    $('.startupSection').fadeOut(300);
-    $('body').css({
-        'overflow':'visible'
-    });
-    /*let audio = document.getElementById('mGS5TPP');
-    audio.play();*/
+function ticking() {
+    let intervalBegin = window.setInterval(beepingIn, 750);
+    let intervalEnd = window.setInterval(beepingOut, 1000);
+    let item = document.getElementById('beepingItem');
+    function beepingIn() {
+        item.style.display = "none";
+    }
+    function beepingOut() {
+        item.style.display = "block"
+    }
+    intervalBegin;
+    intervalEnd;
 }
-
+function testing() {
+    let dateCurrent = new Date();
+    let dateRelease = new Date("04/16/2020");
+    let Difference = dateRelease.getTime() - dateCurrent.getTime();
+    let Result = Math.floor(Difference / (1000 * 3600 * 24));
+    console.log(Result);
+    document.getElementById('releaseDate').innerHTML = Result;
+}
